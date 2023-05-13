9bd0d2db6c0ca5419360146eb2fcaafc168dbfc7
SidonieBouthors <sidonie.bouthors@epfl.ch>
2023-05-13 14:48:09 +0200

    About Page update, Error 404 Page creation

--- components/footer.js
+++ components/footer.js
@@ -4,35 +4,42 @@ class Footer extends HTMLElement {
     }
     connectedCallback() {
     this.innerHTML = `
-        <footer>
-            <div class="container">
-                <div class="col-3">
-                    <p>
-                        Hope you like this website and found what you were looking for!
-                    </p>
-                    <p>
-                        Here are some of my favorite YouTube Channels related to cooking & programming if you'd like to check it out.
-                    </p>
-                </div>
-                <div class="col-1">
-                    <ul class="unstyled-list">
-                        <li><strong>Cooking Channels</strong></li>
-                        <li><a href="https://www.youtube.com/user/FrenchGuyCooking">Alex</a></li>
-                        <li><a href="https://www.youtube.com/channel/UChBEbMKI1eCcejTtmI32UEw">Joshua Weissman</a></li>
-                        <li><a href="https://www.youtube.com/user/BrothersGreenEats">Pro Home Cooks</a></li>
-                    </ul>
-                </div>
-
-                <div class="col-1">
-                    <ul class="unstyled-list">
-                        <li><strong>Science Channels</strong></li>
-                        <li><a href="https://www.youtube.com/channel/UCYO_jab_esuFRV4b17AJtAw">3Blue1Brown</a></li>
-                        <li><a href="https://www.youtube.com/channel/UC4JX40jDee_tINbkjycV4Sg">Tech with Tim</a></li>
-                        <li><a href="https://www.youtube.com/user/keeroyz">Two Minute Papers</a></li>
-                    </ul>
-                </div>
+    <footer>
+        <div class="container footer-container">
+            <div class="footer-col">
+                <p>
+                    Hope you like this website and found what you were looking for!
+                </p>
+                <p>
+                    ⓒ Sidonie Bouthors 2021. All rights reserved
+                </p>
             </div>
-        </footer>`;
+            
+
+            <div class="footer-col">
+                <h3>Social Media</h3>
+                <ul class="unstyled-list">
+                    <li ><a href="">
+                        <img class="footer-icon"
+                            src="../img/icons/linkedin-logo-round.svg"> LinkedIn
+                    </a></li>
+                    <li><a href="">
+                        <img class="footer-icon"
+                        src="../img/icons/telegram-logo-round.svg"> Telegram
+                    </a></li>
+                    <li><a href="">
+                        <img class="footer-icon"
+                        src="../img/icons/github-logo-round.svg"> GitHub EPFL
+                    </a></li>
+                    <li><a href="">
+                        <img class="footer-icon"
+                        src="../img/icons/github-logo-round.svg"> GitHub Personnal
+                    </a></li>
+                    
+                </ul>
+            </div>
+        </div>
+    </footer>`;
     }
 }
 customElements.define('footer-component', Footer);
