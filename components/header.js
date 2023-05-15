09b2651f09f6837e79030859efc3df1b796dd799
SidonieBouthors <sidonie.bouthors@epfl.ch>
2023-05-15 17:22:35 +0200

    Various Updates

--- components/header.js
+++ components/header.js
@@ -3,27 +3,27 @@ class Header extends HTMLElement {
         super();
     }
     connectedCallback() {
-        if (this.getAttribute("hero")==null){
+        if (this.getAttribute("hero")=="no-hero"){
             this.innerHTML = `
             <header>
             <img src="../img/logo.svg" alt="SidoniePyLogo" class="logo">
             <nav>
                 <ul>
-                    <li><a href="http://www.sidonie.me">Home</a></li>
+                    <li><a href="/">Home</a></li>
                     <li><a href="/recipes">Recipes</a></li>
                     <li><a href="/projects">Projects</a></li>
                     <li><a href="/contact">Contact</a></li>
                 </ul>
             </nav>
             </header>`;
-        }
-        else {
+        } 
+        else if (this.getAttribute("hero") != null) {
             this.innerHTML = `
             <header>
             <img src="../img/logo.svg" alt="SidoniePyLogo" class="logo">
             <nav>
                 <ul>
-                    <li><a href="http://www.sidonie.me">Home</a></li>
+                    <li><a href="/">Home</a></li>
                     <li><a href="/recipes">Recipes</a></li>
                     <li><a href="/projects">Projects</a></li>
                     <li><a href="/contact">Contact</a></li>
@@ -32,6 +32,29 @@ class Header extends HTMLElement {
             </header>
 
             <section class="page-hero ${this.getAttribute("hero")}">
+                <div class="container">
+                    <h1 class="title">${this.title}</h1>
+                </div>
+            </section>`;
+        }
+        else {
+            this.innerHTML = `
+            <header>
+            <img src="../img/logo.svg" alt="SidoniePyLogo" class="logo">
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/recipes">Recipes</a></li>
+                    <li><a href="/projects">Projects</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>
+            </nav>
+            </header>
+
+            <section class="page-hero"
+                    style="background-image: url('${this.getAttribute("bg")}');
+                    background-color:rgba(0,0,0, 0.6);
+                    background-blend-mode: overlay;">
                 <div class="container">
                     <h1 class="title">${this.title}</h1>
                 </div>
